ce5db9606911a830d115e80f28e037506ac07d26
AG <148265815+IamVeryGucciMan@users.noreply.github.com>
2024-01-31 14:39:00 -0500

    Add files via upload

--- student_clicker_game/scripts/script.js
+++ student_clicker_game/scripts/script.js
@@ -3,6 +3,15 @@ let score = 0;
 
 // Buildings are made from classes here.
 let bagel = new Building('bakeBagelButton', 'Bagel', 15, 0.1);
+let croissant = new Building('bakeCroissantButton', 'Croissant', 100, 1);
+let doughnut = new Building('bakeDoughnutButton', 'Doughnut', 1100, 8);
+let cinnamonRoll = new Building(
+    'bakeCinnamonRollButton',
+    'Cinnamon Roll',
+    12000,
+    47
+);
+let pie = new Building('bakePieButton', 'Pie', 130000, 260);
 
 // Manual click function, add one point to score per click
 function scorePlusPlus() {
@@ -13,5 +22,9 @@ function scorePlusPlus() {
 function updatePage() {
     document.getElementById('score').innerHTML = score;
     bagel.buttonState();
+    croissant.buttonState();
+    doughnut.buttonState();
+    cinnamonRoll.buttonState();
+    pie.buttonState();
 }
 setInterval(updatePage, tickRate);
